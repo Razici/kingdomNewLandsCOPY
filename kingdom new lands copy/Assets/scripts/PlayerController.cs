272a00a8a7aaaa47cdbd6a228773c95c44a80772
razicel <andronachir@gmail.com>
2018-12-12 17:59:56 +0200

    add movement

--- kingdom new lands copy/Assets/scripts/PlayerController.cs
+++ kingdom new lands copy/Assets/scripts/PlayerController.cs
@@ -6,20 +6,38 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     public Rigidbody2D rb2d;
+    public int direction;
+
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-
+        direction = 1;
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.anyKey)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-        transform.Translate(movement * speed);
-
-
+            if (moveHorizontal < 0 && direction== 1)
+            {
+                direction = -1;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }else
+                if (moveHorizontal > 0 && direction == -1)
+            {
+                direction = 1;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            transform.Translate(movement * speed * direction);
+            anim.SetTrigger("gallop");
+            return;
+        }
+        anim.SetTrigger("idle");
     }
 }
